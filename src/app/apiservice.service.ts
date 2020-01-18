b3bd63b8e47b7b4296f91413572784a7bf3073ff
Fraisin <appletree199@gmail.com>
2020-01-18 18:40:54 -0500

    Added image

--- src/app/apiservice.service.ts
+++ src/app/apiservice.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ApiserviceService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) { }
-
-  public getSummary(searchstring : string){
-    searchstring = searchstring.split(" ").join("_")
-    return this.httpClient.get('https://en.wikipedia.org/api/rest_v1/page/summary/'+ searchstring)
+  public getSummary(searchstring: string) {
+    searchstring = searchstring.split(" ").join("_");
+    return this.httpClient.get(
+      "https://en.wikipedia.org/api/rest_v1/page/summary/" + searchstring
+    );
   }
-
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,26 @@
-import { Component } from '@angular/core';
-import { ApiserviceService } from './apiservice.service';
+import { Component } from "@angular/core";
+import { ApiserviceService } from "./apiservice.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'skeleton';
+  title = "skeleton";
 
-  constructor(private apiService: ApiserviceService) { }
-  
+  constructor(private apiService: ApiserviceService) {}
+
   searchterm;
   summary;
   buttonclick() {
-      this.apiService.getSummary(this.searchterm.toLowerCase()).subscribe((data)=>{
-        this.summary=data['extract']
+    this.apiService.getSummary(this.searchterm.toLowerCase()).subscribe(
+      data => {
+        this.summary = data["extract"];
       },
       err => {
-        this.summary='We could not find any results for the given search'
-      });
-    }
+        this.summary = "We could not find any results for the given search";
+      }
+    );
+  }
 }
